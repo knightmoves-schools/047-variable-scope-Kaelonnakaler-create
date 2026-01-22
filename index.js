12f85a58f82e3d2e03359198fc579a4f86539b49
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-01-22 15:07:28 -0600

    Add distance and estimated arrival time calculation
    Fix syntax errors and calculate estimated arrival time.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+let distanceInMiles = 100;
+estimatedArrivalTime function = 5;
+let milesPerHour (estimatedArrivalTime) = 50;
+estimatedArrivalTime function = a/b;
+document.getElementById("result").innerHTML = estiamtedArrivalTime (5);
 
