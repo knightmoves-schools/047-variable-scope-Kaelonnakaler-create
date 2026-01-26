ba41f789b41996427f8d1362d15d8b9a9720ca84
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-01-26 14:54:05 -0600

    check to see whats missing
    Refactor distance calculation logic and fix syntax errors.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 let distanceInMiles = 100;
-estimatedArrivalTime function = 5;
+let estimatedArrivalTime function(){
+  
 let milesPerHour (estimatedArrivalTime = 50);
 distanceInMiles/milesPerHour = a/b;
-document.getElementById("result").innerHTML = estiamtedArrivalTime (5);
+}
+document.getElementById("result").innerHTML = estimatedArrivalTime (5);
 
 
+//should create a global variable named distanceInMiles and set it to 100
+//should create a function called estimatedArrivalTime that has no parameters
+//should create a local vairable named milesPerHour within estimatedArrivalTime that is set to 50
+//should return the distanceInMiles divided by the milesPerHour from the estimatedArrivalTime function
+//should assign the innerHTML of the HTML element with the id result to return value from estimatedArrivalTime
