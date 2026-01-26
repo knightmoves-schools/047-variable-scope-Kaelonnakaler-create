ae37c4dc34f1d2b88c68571928f6482575c3a01e
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-01-26 14:36:20 -0600

    check

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let distanceInMiles = 100;
 estimatedArrivalTime function = 5;
-let milesPerHour (estimatedArrivalTime) = 50;
-estimatedArrivalTime function = a/b;
+let milesPerHour (estimatedArrivalTime = 50);
+distanceInMiles/milesPerHour = a/b;
 document.getElementById("result").innerHTML = estiamtedArrivalTime (5);
+
 
