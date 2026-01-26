da3b7ecae37c34cb0b6c3769ed576010292eb4b5
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-01-26 14:55:57 -0600

    result

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let estimatedArrivalTime function(){
 let milesPerHour (estimatedArrivalTime = 50);
 distanceInMiles/milesPerHour = a/b;
 }
-document.getElementById("result").innerHTML = estimatedArrivalTime (5);
+document.getElementById("result").innerHTML = estimatedArrivalTime;
 
 
 //should create a global variable named distanceInMiles and set it to 100
@@ -12,3 +12,4 @@ document.getElementById("result").innerHTML = estimatedArrivalTime (5);
 //should create a local vairable named milesPerHour within estimatedArrivalTime that is set to 50
 //should return the distanceInMiles divided by the milesPerHour from the estimatedArrivalTime function
 //should assign the innerHTML of the HTML element with the id result to return value from estimatedArrivalTime
+
